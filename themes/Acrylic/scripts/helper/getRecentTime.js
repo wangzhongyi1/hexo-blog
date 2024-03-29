a0bcaa2fd61f2744323d89bd6a0ffb22da1a91ab
wangzy <wangzy@jackyun.com>
2024-03-29 19:00:06 +0800

    feat: 获取当前机器所属时区

--- themes/Acrylic/scripts/helper/getRecentTime.js
+++ themes/Acrylic/scripts/helper/getRecentTime.js
@@ -1,15 +1,17 @@
-const moment = require('moment')
+const moment = require("moment");
 
-moment.locale('zh-CN')
+moment.locale("zh-CN");
 // 文件创建时间date是东八区时间，但在其他电脑上访问需要处理成对应时区的时间
-function format(date, i = 8) {
+function format(date) {
+  // 获取当前机器所属时区
+  const timeZone = 0 - new Date().getTimezoneOffset() / 60;
   let d = new Date(date);
   let len = d.getTime();
   let offset = d.getTimezoneOffset() * 60000; // 本地时间与GMT时间的偏移差
   let utcTime = len + offset; // 格尼威治时间
-  return new Date(utcTime + 3600000 * i);
+  return new Date(utcTime + 3600000 * timeZone);
 }
 
 hexo.extend.helper.register("recent_data", function (date) {
-    return moment(format(date)).fromNow();
+  return moment(format(date)).fromNow();
 });
